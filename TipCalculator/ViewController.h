ded025f0e98da2f03598523145cc7288ae4ae61e
Puneet Makkar <pmakkar@yahoo-inc.com>
2015-09-12 12:57:02 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Puneet Makkar on 9/12/15.
+//  Copyright (c) 2015 Puneet Makkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Puneet Makkar on 9/12/15.
+//  Copyright (c) 2015 Puneet Makkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
